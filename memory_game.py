8d864a4b8fff723430ee03f9ecf2081cf4357167
Łukasz Rydzewski <luk.rydzewski@gmail.com>
2021-01-23 13:41:07 +0100

    Added get_radnom_letter()

--- memory_game.py
+++ memory_game.py
@@ -1,10 +1,14 @@
 import os
+import random
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # clears the screen
 def console_clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_random_letter():
+    return alphabet[random.randrange(0, len(alphabet))]
 
 def main():
     # write your code here
